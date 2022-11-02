1bdc720f0b9827e25e3b86acc3343e6d5d1b421b
Oscar <46444905+Gogthegog@users.noreply.github.com>
2022-11-02 13:54:36 +0100

    Sanitize rest of queries

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ def notes():
     
     db = connect_db()
     c = db.cursor()
-    statement = "SELECT * FROM notes WHERE assocUser = %s;" %session['userid']
-    print(statement)
-    c.execute(statement)
+    statement = "SELECT * FROM notes WHERE assocUser = %s;"
+    print(statement, (session['userid']),)
+    c.execute(statement, (session['userid']),)
     notes = c.fetchall()
     print(notes)
     
@@ -113,8 +113,8 @@ def login():
         password = request.form['password']
         db = connect_db()
         c = db.cursor()
-        statement = "SELECT * FROM users WHERE username = '%s' AND password = '%s';" %(username, password)
-        c.execute(statement)
+        statement = "SELECT * FROM users WHERE username = '%s' AND password = '%s';"
+        c.execute(statement, (username), (password),)
         result = c.fetchall()
 
         if len(result) > 0:
@@ -140,22 +140,22 @@ def register():
         password = request.form['password']
         db = connect_db()
         c = db.cursor()
-        pass_statement = """SELECT * FROM users WHERE password = '%s';""" %password
-        user_statement = """SELECT * FROM users WHERE username = '%s';""" %username
-        c.execute(pass_statement)
+        pass_statement = """SELECT * FROM users WHERE password = '%s';"""
+        user_statement = """SELECT * FROM users WHERE username = '%s';"""
+        c.execute(pass_statement, (password),)
         if(len(c.fetchall())>0):
             errored = True
             passworderror = "That password is already in use by someone else!"
 
-        c.execute(user_statement)
+        c.execute(user_statement, (username),)
         if(len(c.fetchall())>0):
             errored = True
             usererror = "That username is already in use by someone else!"
 
         if(not errored):
-            statement = """INSERT INTO users(id,username,password) VALUES(null,'%s','%s');""" %(username,password)
-            print(statement)
-            c.execute(statement)
+            statement = """INSERT INTO users(id,username,password) VALUES(null,'%s','%s');"""
+            print(statement, (username), (password),)
+            c.execute(statement, (username), (password),)
             db.commit()
             db.close()
             return f"""<html>
